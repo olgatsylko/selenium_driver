779542699c039b58e216c92884ee5da34d0014ca
Olga Tsylko <helgatsylko@gmail.com>
2021-11-15 21:00:24 +0300

    updated

--- spec/test/test.js
+++ spec/test/test.js
@@ -2,30 +2,23 @@ const {Builder, By} = require('selenium-webdriver');
 require ('chromedriver');
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
 
-describe ('desc', function(){
+describe ('Test web site - navigation', function(){
 
-    it('Test', async function(){
+    it('Test 1', async function(){
         let driver = await new Builder().forBrowser('chrome').build();
         driver.manage().window().maximize();
         async function waitTitle(){
             const titles = await driver.findElements(By.css('.CDt4Ke .Rn3Z1b'));
             return !!titles.length;
         }
-
         await driver.get("https://chromedriver.chromium.org/home");
         await driver.wait(waitTitle, 5000);
         const title1 = await driver.findElement(By.css('.CDt4Ke .Rn3Z1b'));
         let text = await title1.getText();
         expect (text).toBe('ChromeDriver')
-        console.log("Passed!");
       
-        // const headerItems = await driver.findElements(By.css('.plFg0c .VsJjtf'));
-        // await headerItems[2].click();
-
-        const headerItem = await driver.findElement(By.css('#WDxLfe > ul > li:nth-child(3) div'));
-        if (headerItem.isEnabled()){
-                headerItem.click();
-        }
+        const headerItems = await driver.findElements(By.css('#WDxLfe .K1Ci7d .PsKE7e'));
+        await headerItems[2].click();
 
         await driver.wait(waitTitle, 5000);
         const title2 = await driver.findElement(By.css('.tyJCtd .Rn3Z1b'));
@@ -36,8 +29,7 @@ describe ('desc', function(){
         text = await title2.getText();
         console.log(text);
         expect(text).toBe('Chrome Extensions');
-        console.log("Finish!");
-              
+      
         await driver.quit();
 
     });
